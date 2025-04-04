ef834babfd5e0c577ecc003479c99f9830536fa1
jadutter <4691511+jadutter@users.noreply.github.com>
2025-04-04 15:49:09 -0400

    set esModuleInterop to true to be able to import lodash modules

--- src/utils.ts
+++ src/utils.ts
@@ -1,20 +1,20 @@
-"use strict";
+'use strict';
 
-import { env, Selection, window } from "vscode";
-const lodashSortBy = require("lodash.sortby");
+import { env, Selection, window } from 'vscode';
+import lodashSortBy from 'lodash.sortby';
 
 export function getCursors(editBuilder): Selection[] {
 	const editor = window.activeTextEditor;
 	const { document, selections } = editor;
 
-	return lodashSortBy(selections, ["start.line", "start.character"]);
+	return lodashSortBy(selections, ['start.line', 'start.character']);
 }
 
 export async function getClipboardLines(): Promise<string[]> {
 	const val = await env.clipboard.readText();
-	if (typeof val !== "string") {
+	if (typeof val !== 'string') {
 		return [];
 	} else {
-		return val.replace("\r\n", "\n").split("\n");
+		return val.replace('\r\n', '\n').split('\n');
 	}
 }
